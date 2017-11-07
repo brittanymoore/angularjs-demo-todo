02059f5f9b7dff1e15841c946fa797704d288c0a
Brittany Moore <moore.brittanyann@gmail.com>
2017-11-07 15:34:57 -0500

    Switched to html5 routing.

--- src/app/app-routing.config.js
+++ src/app/app-routing.config.js
@@ -1,10 +1,14 @@
-const config = ($stateProvider, $urlRouterProvider) => {
+const config = ($stateProvider, $urlRouterProvider, $locationProvider) => {
+
+    $locationProvider.html5Mode(true);
+    
     $urlRouterProvider.otherwise('/');
     $stateProvider
         .state('todo', { url: '/', component: 'todo' });
+
 };
 
-config.$inject = [ '$stateProvider', '$urlRouterProvider' ];
+config.$inject = [ '$stateProvider', '$urlRouterProvider', '$locationProvider' ];
 
 export const routes = {
     config: config
